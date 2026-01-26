db253cc6d9e747eef5f092fa85c319ba7cd27e4b
FredLuc12 <ablefonlin.f@yahoo.com>
2026-01-26 14:37:31 +0100

    Templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
-def dashboard_view(request):
-    return render(request, 'dashboard/dashboard.html')
-
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from .models import SensorData
 
+
+@login_required
+def dashboard_view(request):
+    return render(request, "dashboard/dashboard.html")
+
+
 def sensors_api(request):
-    data = list(SensorData.objects.values().order_by('-created_at')[:10])
+    data = list(SensorData.objects.values().order_by("-created_at")[:10])
     return JsonResponse(data, safe=False)
